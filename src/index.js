308967b921ff53f07ef4725c56d2ca61e83b008f
topin <topin@kkwindsolutions.com>
2023-03-24 10:21:54 +0100

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const myFirstElement = <h1>Hello React!</h1>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root = ReactDOM.createRoot(document.getElementById('root')); // the html element with the id 'root' is the root of the DOM tree. It is located in a div tag in body. This element is not created by the user.
+root.render(myFirstElement);
